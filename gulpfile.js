bfb2fe34e852fc9f294001d11a58b92104b79cd6
Alex <skarppi@mail.ru>
2016-02-29 18:51:49 +0300

    Simplified gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,34 +1,72 @@
 'use strict';
 
-var gulp = require('gulp');
-var wrench = require('wrench');
 
 /**
- *  This will load all js and coffee files in the gulp directory
- *  in order to load all gulp tasks
+ *  Config
  */
-wrench.readdirSyncRecursive('./gulp').filter(function(file) {
-  return (/\.(js|coffee)$/i).test(file);
-}).map(function(file) {
-  require('./gulp/' + file);
-});
+var conf = {
+  paths: {
+    src: 'src',
+    dist: 'dist',
+    tests: 'spec'
+  },
+  names: {
+    main: 'esri-leaflet-dynamic-advanced'
+  }
+};
 
 
 /**
- *  Default task clean temporaries directories and launch the
- *  main optimization build task
+ *  Modules
+ */
+var path = require('path');
+var karma = require('karma');
+var gulp = require('gulp');
+var gutil = require('gulp-util');
+var rename = require('gulp-rename');
+var uglify = require('gulp-uglify');
+var del = require('del');
+
+
+/**
+ *  Build tasks
  */
 gulp.task('default', ['clean'], function() {
-  gulp.start('build');
+  gulp.start('test and build');
 });
 
-//
-//gulp.task('default', function () {
-//  return gulp.src('src/DynamicMapLayerAdvanced.js')
-//
-//    .pipe(rename({ basename: 'esri-leaflet-dynamic-advanced' }))
-//    .pipe(gulp.dest(DEST))
-//    .pipe(uglify())
-//    .pipe(rename({ extname: '.min.js' }))
-//    .pipe(gulp.dest(DEST));
-//});
+gulp.task('clean', function() {
+  return del(path.join(conf.paths.dist, '/'));
+});
+
+gulp.task('build', function() {
+  return gulp.src(path.join(conf.paths.src, '/**/*.js'))
+    .pipe(rename({basename: conf.names.main}))
+    .pipe(gulp.dest(path.join(conf.paths.dist, '/')))
+    .pipe(uglify()).on('error', errorHandler('Uglify'))
+    .pipe(rename({extname: '.min.js'}))
+    .pipe(gulp.dest(path.join(conf.paths.dist, '/')));
+});
+
+
+/**
+ *  Test tasks
+ */
+gulp.task('test and build', ['build'], function(done) {
+  new karma.Server({
+    configFile: path.join(__dirname, '/karma.conf.js')
+  }, function(failCount) {
+    done(failCount ? new Error("Failed " + failCount + " tests.") : null);
+  }).start();
+});
+
+
+/**
+ *  Common implementation for an error handler of a Gulp plugin
+ */
+function errorHandler(title) {
+  return function(err) {
+    gutil.log(gutil.colors.red('[' + title + ']'), err.toString());
+    this.emit('end');
+  };
+}
